8ae9acd5962a492e3812b2eb5bc7c959d84125a2
Sayed <sayedhossainsifat300@gmail.com>
2021-12-23 05:26:36 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,28 @@
+// // let pakhi ;
+// // // console.log(pakhi);
+
+// // function add (num1,num2) {
+// //     console.log(num1 + num2);
+// //     return 
+// // };
+
+// // const result = add(12,12);
+// // console.log(result);
+
+// function add (num1, num2) {
+//     console.log(num2);
+// };
+// const result = add(12);
+// console.log(result);
+
+// const lover = {name1 : "Sifat", phone : "045622141"};
+// console.log(lover.job);
+
+let fun = undefined ;
+console.log(fun);// undefined  means a negative or falsy ;
+
+// null 
+// null means not existance
+
+let ages = [1,2,3];
+console.log(ages[5]);
