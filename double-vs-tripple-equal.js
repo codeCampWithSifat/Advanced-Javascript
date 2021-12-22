1e502c6ab66eeaef7c7b3695570f2c4cbb4b5181
Sayed <sayedhossainsifat300@gmail.com>
2021-12-23 05:41:59 +0600

    double vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,17 @@
+// const first = 2 ;
+// const second = "2";
+// if (first == second) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+
+// the benefits of the tripple equal 
+
+const first = 2 ;
+const second = "2";
+if (first === second) {
+    console.log("condition is true");
+} else {
+    console.log("conditon is false");
+}
