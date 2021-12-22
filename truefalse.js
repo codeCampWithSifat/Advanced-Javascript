eda22ed67628727298ab3b2b5ba580de0d7e6ab1
Sayed <sayedhossainsifat300@gmail.com>
2021-12-23 05:09:48 +0600

    true or false

---
+++ truefalse.js
@@ -0,0 +1,17 @@
+
+// const name = "0"
+// javascript falsy value ;
+// 0
+// ""
+// null
+// undefined
+// NaN 
+// false 
+
+let name2 ;
+console.log(name2);
+if (name2) {
+    console.log("condition is true")
+} else {
+    console.log("condition is false")
+}
