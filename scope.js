97b2563d47378f2a406d5a0e4b9ecebd2203edb8
Sayed <sayedhossainsifat300@gmail.com>
2021-12-23 22:50:15 +0600

    kichi siksi

--- scope.js
+++ scope.js
@@ -1,5 +1,13 @@
+let bonous = 20 ;
+
+
 function add (num1 , num2) {
-    let result = num1 + num2 ;
+    let result = num1 + num2 + bonous ;
+    // console.log(bonous) ;
+    if (result > 9) {
+        const mood = "happy";
+        console.log(mood)
+    }
     return result ;
 };
 
